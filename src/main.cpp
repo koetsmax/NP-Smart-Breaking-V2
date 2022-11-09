41f9544580ca7e47d293cd95e83cfed462c24dbc
Max Koets <koetsmax@gmail.com>
2022-11-09 15:36:04 +0100

    Formatting veranderd

--- src/main.cpp
+++ src/main.cpp
@@ -6,51 +6,57 @@ int count = 0;
 float diavr = 0.25;
 int rotation = 0;
 
-//time 
+// time
 int ctime = millis();
 int interval = 10;
 // Tracks the time since last event fired
 unsigned long previousMillis = 0;
 
-//sensor
+// sensor
 int sensorVoor = (32);
 int valA = 0;
 
-void setup() {
-  Serial.begin(115200);
+void setup()
+{
+    Serial.begin(115200);
 }
 
-int snelheid(int dis, int currentMillis) {
-  int speed = (dis / currentMillis);
-  return speed;
+int snelheid(int dis, int currentMillis)
+{
+    int speed = (dis / currentMillis);
+    return speed;
 }
 
-void loop() {
+void loop()
+{
+    int movement = analogRead(sensorVoor);
+    int dis = rotation * (diavr * pi);
+    // Get snapshot of time
+    unsigned long currentMillis = millis();
 
- int movement = analogRead(sensorVoor);
- int dis = rotation *(diavr * pi);
- // Get snapshot of time
- unsigned long currentMillis = millis();
+    valA = analogRead(sensorVoor);
+    if (valA > 1)
+    {
+        valA = 1;
+    }
+    Serial.println(valA);
 
- valA = analogRead(sensorVoor);
-  if( valA > 1 ){
-   valA = 1;}
-  Serial.println(valA);
+    if (valA > 1)
+    {
+        count++;
+    }
 
- if (valA > 1){
-  count++;
- }
+    if (count == 3)
+    {
+        count = 0;
+        rotation++;
+    }
 
- if (count == 3){ 
-  count = 0;
-  rotation++;
- }
+    int dis = rotation * (diavr * pi);
 
- int dis = rotation * (diavr * pi);
-
- // How much time has passed, accounting for rollover with subtraction!
- if ((unsigned long)(currentMillis - previousMillis) >= interval) { 
-  previousMillis = currentMillis;
- 
- }
+    // How much time has passed, accounting for rollover with subtraction!
+    if ((unsigned long)(currentMillis - previousMillis) >= interval)
+    {
+        previousMillis = currentMillis;
+    }
 }
