2d2fcaa470ffa8262a2d9bfa41e1803f804a5b95
Max Koets <koetsmax@gmail.com>
2023-01-25 15:47:54 +0100

    rotation werkt jippieeeeeeeeeeeeeeeeeeeeeee

--- README.md
+++
@@ -1,3 +0,0 @@
-# NP-Smart-Breaking-Project
-
-Dit is onze git repo voor ons school-project.
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
 #include <Arduino.h>
-#include <mat.h>
 
 // creeer cons.0dretant variables
 float pi = 3.1415926535;
@@ -12,76 +11,87 @@ int interval = 5000;
 // Tracks the time for how long the program has been running for
 unsigned long currentMillis = 0;
 unsigned long previousMillis = 0;
-// Datapin waar de voorsensor is aangesloten
-int sensorVoorChannelA = 32;
-int sensorVoorChannelB = 25;
-// De huidige waarde van de sensor
-int valSensorVoor = 0;
 
-int check_if_driving()
-{
-    // check if 5 seconds have passed without a change in the value
-    if (currentMillis - previousMillis >= interval)
-    {
-        previousMillis = currentMillis;
-        Serial.println("We staan al 5 seconden stil");
-    }
-}
+// Datapin waar de voorsensor is aangesloten (channel A)
+int sensorVoorChannelA = A0;
+int sensorVoorChannelB = A1;
+// De huidige waarde van de sensor (channel A)
 
 void setup()
 {
-    Serial.begin(115200);
+    Serial.begin(9600);
 }
 
-int snelheid(int distance, int currentMillis)
-{
-    int speed = (distance / currentMillis);
-    return speed;
-}
+// int snelheid(int distance, int currentMillis)
+// {
+//     int speed = (distance / currentMillis);
+//     return speed;
+// }
 
 void loop()
 {
-    unsigned long currentMillis = millis();
-    // int checkValueA = analogRead(sensorVoorChannelA);
+    int checkValueA = analogRead(sensorVoorChannelA);
     int checkValueB = analogRead(sensorVoorChannelB);
-
-    if (checkValueB > 1)
+    if (checkValueA && checkValueB > 20)
     {
-        Serial.println(checkValueB);
-        check_if_driving();
+        Serial.println("vooruit rondje gemaakt");
+        count++;
+        Serial.print("count: ");
+        Serial.println(count);
     }
-    else
-    {
-        Serial.println("De waarde van de sensor is nul");
-        check_if_driving();
-    }
+    // Serial.print("checkValueA =");
+    // Serial.print(checkValueA);
+    // Serial.print(" en checkValueB =");
+    // Serial.println(checkValueB);
 
+    // int checkValueB = analogRead(sensorVoorChannelB);
+
+    // digital omzetten
+    // als de waarde groter is dan 1 zet de waarde naar 1 anders naar 0
+    // if (checkValueA > 1)
+    // {
+    //     checkValueA = 1;
+    // }
+    // Serial.print("Sensor waarde is")
+    // else
+    // {
+    //     checkValueA = 0;
+    // }
+
+    // int previousValue = checkValueA;
+
+    // if (previousValue == 0)
+    // {
+    //     // 1 rotatie is voltooid
+    //     if (count == 3)
+    //     {
+    //         count = 0;
+    //         Serial.println("rondje gemaakt");
+    //         Serial.print("count: ");
+    //         Serial.println(count);
+    //         delay(1000);
+    //     }
+    //     else
+    //     {
+    //         count++;
+    //         Serial.print("count: ");
+    //         Serial.println(count);
+    //         delay(1000);
+    //     }
+    // }
+    // Serial.println("hallo");
+
+    // Serial.println(checkValueA);
     // int distance = rotation * (diavr * pi);
     // // Get snapshot of time
-
-    // valSensorVoor = analogRead(sensorVoor);
-    // // zet de waardevan de voor sensor naar 1 als de waarde groter is dan 1
-    // if (valSensorVoor > 1)
-    // {
-    //     Serial.println("waarde is hoger dan " + String(valSensorVoor));
-    //     // valSensorVoor = 1;
-    //     count++;
-    // }
-    // // Serial.println(valSensorVoor);
-
-    // // als de count groter is dan 3
-    // if (count == 3)
-    // {
-    //     count = 0;
-    //     rotation++;
-    //     // Serial.println("rotation");
-    // }
+    //     unsigned long currentMillis = millis();
 
     // How much time has passed, accounting for rollover with subtraction
     // if the time since the last loop is greater than the interval
 
     // if (currentMillis - previousMillis >= interval)
     // {
+    //     // save the last time you blinked the LED
     //     previousMillis = currentMillis;
     //     Serial.println("distance: " + distance);
     //     Serial.println("snelheid: " + snelheid(distance, currentMillis));
